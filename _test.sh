d30f5ddcd5fdcd63998695fb1bf05ac14a5eccd2
ShinNakamura <kokoronopython@gmail.com>
2022-06-02 08:26:17 +0900

    src/bin下に作った個別の実行ファイルを指定してrunする

--- _test.sh
+++ _test.sh
@@ -2,7 +2,7 @@
 unalias -a
 
 expect="aaa"
-act="$(echo "aaa | oooo" | cargo run -- "|" 0)"
+act="$(echo "aaa | oooo" | cargo run --bin cutbystr -- "|" 0)"
 if test "_$expect" = "_$act"
 then
     echo "pass"
